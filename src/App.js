e8849f30ecb4c918ae4f33d1e4d0372e101ce5a4
Pete Lindsell <pete.lindsell@gmail.com>
2021-05-16 19:02:53 +0100

    Clean up app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import axios from "axios";
 import styled from "styled-components";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/shared/header";
-import Hero from "./components/hero";
-import Section from "./layout/section";
-import Wrapper from "./layout/wrapper";
-import LaunchCard from "./components/lauch-card";
-import Grid from "./components/grid";
 import Launches from "./pages/launches";
 import Rockets from "./pages/rockets";
 import Home from "./pages/home";
@@ -20,38 +13,7 @@ const MainWrapper = styled.main`
   width: 100%;
 `;
 
-const ContentSelector = styled.div`
-  width: 80%;
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-
-  button {
-    border: none;
-    padding: 10px;
-    min-width: 100px;
-    margin-right: 10px;
-  }
-`;
-
 function App() {
-  const [data, setData] = useState({ launches: [] });
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        "https://api.spacexdata.com/v4/launches/past?limit=10"
-      );
-
-      console.log(result.data);
-
-      setData({ launches: result.data });
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
-
   return (
     <Router>
       <MainWrapper>
@@ -67,13 +29,6 @@ function App() {
             <Home />
           </Route>
         </Switch>
-
-        {/* <Section>
-          <ContentSelector>
-            <button>Launches</button>
-            <button>rockets</button>
-          </ContentSelector>
-        </Section> */}
       </MainWrapper>
     </Router>
   );
